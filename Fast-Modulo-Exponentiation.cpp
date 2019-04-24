20785dbb6b213b2a3caf7e60860fa5a76e6d7918
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-04-24 10:57:44 +0530

    Add files via upload

---
+++ Fast-Modulo-Exponentiation.cpp
@@ -0,0 +1,28 @@
+// Fast Modulo Exponentiation
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+ll moduloExpo(ll a, ll b, ll MOD){
+
+	ll res=1;
+
+	while(b){
+
+		if(b&1) res=(res*a)%MOD;
+		a=(a*a)%MOD;
+		b>>=1;
+	}
+	return res;
+}
+
+int main(){
+
+	ll a,b,MOD;
+	a=5,b=3,MOD=10000007;
+
+	cout<<moduloExpo(a,b,MOD);
+
+	return 0;
+}
